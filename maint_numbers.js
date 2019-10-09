c61fe4fac97fd1f7f93f111fa69bc29c5e6fd1ae
sebastianmartinez <sebastianmartinbez@seven4n.co>
2019-10-09 10:57:54 -0500

    Number types

---
+++ maint_numbers.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var avengers = 6;
+var villanos; //undefined
+var otros = 2;
+if (avengers > villanos) {
+    console.log("Saved");
+}
+else {
+    console.log("NOOOOOO");
+}
---
+++ maint_numbers.ts
@@ -0,0 +1,10 @@
+export {}; //Variable colchon para evitar salida de errores
+let avengers:number = 6;
+let villanos:number; //undefined
+let otros = 2;
+
+if (avengers>villanos){
+    console.log("Saved")
+}else{
+    console.log("NOOOOOO")
+}

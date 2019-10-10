87e0005b465ffe2b4736ae223cbb9c9a49da275c
sebastianmartinez <sebastianmartinbez@seven4n.co>
2019-10-10 09:24:49 -0500

    Any in Typescript

---
+++ main_any.js
@@ -0,0 +1,11 @@
+var vengador;
+var existe = true;
+existe = "sí";
+var derrotas;
+//Toda variable no inicializada y no tipada, por defecto será tipo Any
+vengador = "Dr. Strange";
+console.log(vengador.charAt(0));
+vengador = 150.45555;
+console.log(vengador.toFixed(2));
+vengador = true;
+console.log(!vengador);
---
+++ main_any.ts
@@ -0,0 +1,14 @@
+let vengador;
+let existe:any=true;
+existe="sí"
+let derrotas;
+
+//Toda variable no inicializada y no tipada, por defecto será tipo Any
+
+vengador="Dr. Strange";
+console.log(vengador.charAt(0));
+vengador = 150.45555;
+console.log(vengador.toFixed(2));
+
+vengador=true;
+console.log(!vengador)

3d77d663dd195c8b5784802de6f25ae7d7b6ec0e
sebastianmartinez <sebastianmartinbez@seven4n.co>
2019-10-10 12:17:12 -0500

    Types example

--- main_objects.js
+++ main_objects.js
@@ -8,4 +8,20 @@ var flash = {
         return this.nombre;
     }
 };
+var supermansito = {
+    nombre: "Clark Kent",
+    edad: 30,
+    poderes: ["Volar", "BS", 30],
+    getNombre: function () {
+        return this.nombre;
+    }
+};
 console.log(flash.getNombre());
+var flash2 = {
+    nombre: "Barry Allen",
+    edad: 24,
+    poderes: ["Correr rapido", "Viajar por el tiempo", 24],
+    getNombre: function () {
+        return this.nombre;
+    }
+};
--- main_objects.ts
+++ main_objects.ts
@@ -1,4 +1,14 @@
 export {}
+
+type Heroe ={
+    nombre:string, 
+    poderes:any[],
+    edad:number,
+    getNombre:()=>string,
+    //heroe: Heroe
+}
+
+
 let flash: {nombre:string, poderes:any[], edad:number, getNombre:()=>string}={
     nombre: "Barry Allen",
     edad: 24,
@@ -8,4 +18,23 @@ let flash: {nombre:string, poderes:any[], edad:number, getNombre:()=>string}={
     }
 }
 
+let supermansito: {nombre:string, poderes:any[], edad:number, getNombre:()=>string}={
+    nombre: "Clark Kent",
+    edad: 30,
+    poderes: ["Volar", "BS", 30],
+    getNombre(){
+        return this.nombre;
+    }
+}
 console.log(flash.getNombre());
+
+
+
+let flash2={
+    nombre: "Barry Allen",
+    edad: 24,
+    poderes: ["Correr rapido", "Viajar por el tiempo", 24],
+    getNombre(){
+        return this.nombre;
+    }
+}

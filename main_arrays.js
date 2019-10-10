4eaabd3308cc1edd7709df8d0d404e01761d7548
sebastianmartinez <sebastianmartinbez@seven4n.co>
2019-10-10 10:00:43 -0500

    Array in TypeScript

---
+++ main_arrays.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var arreglo = [1, 2, 3, 4, 5];
+arreglo.push("6"); //NO NO NO, ERROR
+var arreglo2 = [1, 2, 34, 5, 6];
+var villanos = ["Dio", "Kars", "Kira"];
+villanos.push("Diavolo");
+console.log(villanos[0].charAt(0));
---
+++ main_arrays.ts
@@ -0,0 +1,11 @@
+export {}
+let arreglo=[1,2,3,4,5];
+arreglo.push("6"); //NO NO NO, ERROR
+
+let arreglo2:number[]=[1,2,34,5,6];
+
+let villanos:string[]=["Dio", "Kars", "Kira"];
+
+villanos.push("Diavolo");
+console.log(villanos[0].charAt(0));
+

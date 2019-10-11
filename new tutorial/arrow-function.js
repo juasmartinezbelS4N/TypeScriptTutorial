09465e6b849a46bea01d6130cbb2463300d8afc7
sebastianmartinez <sebastianmartinbez@seven4n.co>
2019-10-11 08:54:46 -0500

    New tutorial started

---
+++ new tutorial/arrow-function.js
@@ -0,0 +1,7 @@
+var fn = function () { return "respuesa"; };
+var a;
+var b;
+var c;
+a = 20;
+b = true;
+c = "hello";
---
+++ new tutorial/arrow-function.ts
@@ -0,0 +1,9 @@
+let fn = () => "respuesa";
+
+let a:number;
+let b:boolean;
+let c:string;
+
+a=20;
+b=true;
+c="hello";

38769122c14df95de97da29cefef08a1ede349bf
sebastianmartinez <sebastianmartinbez@seven4n.co>
2019-10-11 09:44:02 -0500

    Arrays and Tuples

--- new tutorial/arrow-function.js
+++ new tutorial/arrow-function.js
@@ -1,3 +1,4 @@
+/** **/
 var fn = function () { return "respuesa"; };
 var a;
 var b;
@@ -5,3 +6,17 @@ var c;
 a = 20;
 b = true;
 c = "hello";
+/**Undefined Type**/
+var foo;
+var foo2; //Existen, pero no sirven pa NaH
+/**Arrays**/
+var myArr = [1, 2, 3];
+var myArr2;
+myArr = [1, 2, 3, 4];
+myArr.push(9);
+console.log(myArr.pop());
+console.log(myArr);
+var myArr3 = [2, 3, "4", { value: 5 }, true, function keke() { return 6; }];
+console.log(myArr3);
+/**Tuples**/
+var myTuple = [1, true];
--- new tutorial/arrow-function.ts
+++ new tutorial/arrow-function.ts
@@ -1,3 +1,4 @@
+/** **/
 let fn = () => "respuesa";
 
 let a:number;
@@ -7,3 +8,19 @@ let c:string;
 a=20;
 b=true;
 c="hello";
+/**Undefined Type**/
+let foo:null;
+let foo2:undefined; //Existen, pero no sirven pa NaH
+
+/**Arrays**/
+var myArr=[1,2,3]
+var myArr2:number[];
+myArr=[1,2,3,4]
+myArr.push(9);
+console.log(myArr.pop());
+console.log(myArr)
+var myArr3:any[]=[2,3,"4",{value:5}, true, function keke(){return 6}]
+console.log(myArr3);
+
+/**Tuples**/
+var myTuple:[number,boolean]=[1,true];
